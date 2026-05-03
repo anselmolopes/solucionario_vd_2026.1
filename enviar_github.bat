acd6e412114b813f63132c6941600490cd5f5cd6
Anselmo Lopes <107046598+anselmolopes@users.noreply.github.com>
2026-05-02 22:27:18 -0300

    Atualizacao de progresso via TeXstudio

--- enviar_github.bat
+++ enviar_github.bat
@@ -1,4 +1,3 @@
-cd /d "D:\Backup de Anselmo 04-02-2026\Area de Trabalho\Doutorado\PPGMAT\Disciplinas\2026.1\Variedades Diferenciáveis\Lista de Exercício 2026.1\Soluções\solucionario_vd_2026.1"
 git add .
 git commit -m "Atualizacao de progresso via TeXstudio"
 git push origin main
